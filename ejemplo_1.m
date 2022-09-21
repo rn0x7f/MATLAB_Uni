22871bc51bc95ac37cf89a5147bd9b5c6b03aa7d
Xoerix <ampheriz0@gmail.com>
2022-09-21 09:48:21 -0500

    prueba

---
+++ ejemplo_1.m
@@ -0,0 +1,14 @@
+clear all; clc;
+%Este programa realiza operaciones
+% a = 3
+% b = -2
+% suma = a + b
+% mult = a.*b
+
+% x = input('Ingresa el primer valor:\n')
+% y = input('Ingresa el segundo valor:\n')
+% sum = x + y
+
+a = input('Ingresa los numeros de tu primer vector\nEjemplo: [1 2 3 4 5]\n');
+b = input('Ingresa los numeros de tu segundo vector\nEjemplo: [1 2 3 4 5]\n');
+sum = a + b
